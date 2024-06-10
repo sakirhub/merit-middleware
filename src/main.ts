b5cf347f8cff1b5573c215a08cc4f6db2f0fd057
sakirhub <151774812+sakirhub@users.noreply.github.com>
2024-06-10 09:40:23 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3001);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap().then(() => console.log('Server is running'));
